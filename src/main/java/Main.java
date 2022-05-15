d9c8c7896ac19c8591925a87bb758cec55c7f799
lokky55 <lokky55@mail.ru>
2022-05-15 16:57:39 +0300

    MathService

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,10 @@
+import ru.netology.javaqa.MathService;
+
 public class Main {
+    public static void main(String[] args) {
+        MathService service = new MathService();
+        int res = service.crazyCount(400);
+        System.out.println(res);
+
+    }
 }
--- src/main/java/ru/netology/javaqa/MathService.java
+++ src/main/java/ru/netology/javaqa/MathService.java
@@ -1,4 +1,14 @@
 package ru.netology.javaqa;
 
 public class MathService {
+    public int crazyCount(int limit) {
+        int count = 0;
+        for (int i = 0; i <= 50; i++) {
+            if (i * 13 < limit) {
+                count++;
+            }
+        }
+        return count;
+    }
 }
+
